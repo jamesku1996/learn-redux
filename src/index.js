f0576d485fabb86c5470f033e857fe7ed79f0bcc
James Ku <jameskutw@gmail.com>
2020-04-08 13:52:19 -0400

    Basic concepts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+
+// STORE -> global state
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = () => {
+  return { type: "DECREMENT" }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1
+    case "DECREMENT":
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
+
+//Display i in the console
+store.subscribe(() => console.log(store.getState()))
+//DISPATCH
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
